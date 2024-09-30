5273bab18f392058f7a54fce17c3e58280bee879
Thunder <thunder-01@ADMIN-01s-iMac.local>
2024-09-30 21:26:23 +0800

    add: lesson day 01

---
+++ lesson_day_01/bin/lesson_day_01.dart
@@ -0,0 +1,5 @@
+import 'package:lesson_day_01/lesson_day_01.dart' as lesson_day_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lesson_day_01.calculate()}!');
+}
---
+++ lesson_day_01/test/lesson_day_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_day_01/lesson_day_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
