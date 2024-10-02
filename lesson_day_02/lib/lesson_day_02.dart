ae48873eff6a9d5015a3255c446d5de619589d37
Thunder <thunder-01@ADMIN-01s-iMac.local>
2024-10-02 21:22:00 +0800

    add: lesson day_02

---
+++ lesson_day_02/lib/lesson_day_02.dart
@@ -0,0 +1,65 @@
+mport 'dart:math';
+
+void main() {
+  print("hh");
+  print(675 % 12);
+  print(650 % 5);
+  print(1234567 % 1234);
+  print(7777 % 10);
+  print(5000 % 300);
+  print(12345789 % 5555);
+  print(((800 / (5 * 10) - 32) ~/ (29 + 5)));
+  print(350 / (5 + 2));
+  print(sin(45 * pi / 180));
+  print(pi);
+  print(sqrt(7));
+  print(max(5, 10));
+  print(max(sqrt(2), pi / 2));
+  print(sqrt(100) + (sqrt(110)) ~/ 1);
+  print(5 * 5 * 3.14);
+  print(sin(45 * pi / 180) + cos(45 * pi / 180));
+  int number = 10;
+  int age = 21;
+  print(age = 21);
+  print(number);
+  double apple = 3.14159;
+  print(apple);
+  int myinteger = 10;
+  print(myinteger);
+  const int speedofligth = 299792458;
+  print(speedofligth);
+  print(speedofligth);
+  const double PI = 3.14159;
+  print(PI);
+  const double choloot_unalt = 100;
+  print(choloot_unalt);
+  const myAge = 21;
+  print(myAge);
+  const double averageAge = (21 + 21) % 2;
+  print(averageAge);
+  const int testNumber = 50;
+  const int evenOdd = testNumber % 2;
+  print(evenOdd);
+  int counter = 0;
+  print(counter);
+  counter += 1;
+  print(counter);
+  counter -= 1;
+  print(counter);
+  counter++;
+  counter--;
+  double myValue = 10;
+  print(myValue);
+  myValue *= 3;
+  print(myValue);
+  myValue /= 2;
+  print(myValue);
+
+
+
+
+
+
+}
+
+
